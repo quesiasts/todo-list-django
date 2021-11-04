8a4f858ab70981ab408a5f3e3bf5d0405ad29ada
Quésia <quesiasts@gmail.com>
2021-11-03 23:35:46 -0300

    changing files

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
-
-# Register your models here.
 from . import models
-
 class TodoListAdmin(admin.ModelAdmin):
 	list_display = ("title", "created", "due_date")
-
 class CategoryAdmin(admin.ModelAdmin):
 	list_display = ("name",)
 
